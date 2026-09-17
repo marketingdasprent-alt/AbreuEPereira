cb20b13e3719b1718db36c9d9a1ad7a4ede97ec2
lhdenkil01 <157542377+lhdenkil01@users.noreply.github.com>
2026-09-17 11:15:31 -0300

    A&P-V1
    A&P Website First Model

--- script.js
+++ script.js
@@ -126,7 +126,7 @@
   */
 
   function ativarAparicoes() {
-    var alvos = document.querySelectorAll(".casa, .ficha, .contacto-in");
+    var alvos = document.querySelectorAll(".casa, .ficha, .faq-in, .contacto-in");
     if (!alvos.length) return;
 
     if (semMovimento || !("IntersectionObserver" in window)) return;
